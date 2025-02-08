78651440a75e36b58e9183742e2f6c99bd278a7b
Dallin Christensen <dallinchristensen2@gmail.com>
2025-02-08 02:27:43 -0700

    Remove broken link and update vite.config.js for github actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "./fonts/real-vhs-font/RealVhsFontRegular.ttf";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
