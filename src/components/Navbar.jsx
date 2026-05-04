71e0cce4f7c155fd1795a96fc09e71e57c54d70b
Dallin Christensen <62360764+fmajorcminor@users.noreply.github.com.>
2026-05-03 20:58:31 -0600

    Add Blog

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,76 @@
 import Resume from "../assets/DallinChristensen-Resume.pdf";
 
 const Navbar = () => {
-  const handleButtonClick = (link) => {
-    window.open(link, "_self");
-  };
+    const handleButtonClick = (link) => {
+        window.open(link, "_self");
+    };
 
-  return (
-    <nav className="px-8 md:px-8 lg:px24">
-      <div
-        className="container-fluid py-2 flex justify-center md:justify-between items-center text-3xl"
-        style={{ fontFamily: "LexingtonGothic" }}
-      >
-        <div className="font-bold text-5xl hidden md:inline">
-          Dallin Christensen
-        </div>
-        <div className="space-x-5">
-          <button
-            onClick={() => {
-              window.open(Resume, "_blank");
-            }}
-            style={{
-              cursor: "pointer",
-              fontSize: "30px",
-              fontFamily: "LexingtonGothic",
-              backgroundColor: "lightgray",
-            }}
-          >
-            Resume
-          </button>
-          <button
-            onClick={() => handleButtonClick("https://github.com/fmajorcminor")}
-            style={{
-              cursor: "pointer",
-              fontSize: "30px",
-              fontFamily: "LexingtonGothic",
-              backgroundColor: "lightgray",
-            }}
-          >
-            GitHub
-          </button>
-          <button
-            onClick={() =>
-              handleButtonClick(
-                "https://www.linkedin.com/in/dallin-christensen-a37903aa/",
-              )
-            }
-            style={{
-              cursor: "pointer",
-              fontSize: "30px",
-              fontFamily: "LexingtonGothic",
-              backgroundColor: "lightgray",
-            }}
-          >
-            LinkedIn
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="px-8 md:px-8 lg:px24">
+            <div
+                className="container-fluid py-2 flex justify-center md:justify-between items-center text-3xl"
+                style={{ fontFamily: "LexingtonGothic" }}
+            >
+                <div className="font-bold text-5xl hidden md:inline">
+                    Dallin Christensen
+                </div>
+                <div className="space-x-5">
+                    <button
+                        onClick={() => {
+                            window.open(Resume, "_blank");
+                        }}
+                        style={{
+                            cursor: "pointer",
+                            fontSize: "30px",
+                            fontFamily: "LexingtonGothic",
+                            backgroundColor: "lightgray",
+                        }}
+                    >
+                        Resume
+                    </button>
+                    <button
+                        onClick={() =>
+                            handleButtonClick("https://github.com/fmajorcminor")
+                        }
+                        style={{
+                            cursor: "pointer",
+                            fontSize: "30px",
+                            fontFamily: "LexingtonGothic",
+                            backgroundColor: "lightgray",
+                        }}
+                    >
+                        GitHub
+                    </button>
+                    <button
+                        onClick={() =>
+                            handleButtonClick(
+                                "https://www.linkedin.com/in/dallin-christensen-a37903aa/",
+                            )
+                        }
+                        style={{
+                            cursor: "pointer",
+                            fontSize: "30px",
+                            fontFamily: "LexingtonGothic",
+                            backgroundColor: "lightgray",
+                        }}
+                    >
+                        LinkedIn
+                    </button>
+                    <button
+                        onClick={() => handleButtonClick("/blog", false)}
+                        style={{
+                            cursor: "pointer",
+                            fontSize: "30px",
+                            fontFamily: "LexingtonGothic",
+                            backgroundColor: "lightgray",
+                        }}
+                    >
+                        Blog
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 import "./index.css";
 import App from "./App.jsx";
 import "98.css/dist/98.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
